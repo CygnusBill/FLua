f6367493b67209fa576871cdd6d261f46640680a
Bill Schulz <none@bogus.com>
2025-07-26 23:54:11 -0400

    chore: Clean up test files and debug symbols

--- test_aot.lua
+++
@@ -1,9 +0,0 @@
--- Simple Lua script to test AOT compilation
-print("Hello from AOT-compiled Lua!")
-print("2 + 2 =", 2 + 2)
-
-local function greet(name)
-    return "Hello, " .. name .. "!"
-end
-
-print(greet("World"))
